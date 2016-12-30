0d262b945e20ce7b2b36fc0a804d2a9a96117b06
JoyceMatos <j.matosjoyce@gmail.com>
2016-12-29 19:28:25 -0500

    Initial commit

---
+++ QuotesForYou/QuoteAPIClient.swift
@@ -0,0 +1,23 @@
+//
+//  QuoteAPIClient.swift
+//  QuotesForYou
+//
+//  Created by Joyce Matos on 12/29/16.
+//  Copyright © 2016 Joyce Matos. All rights reserved.
+//
+
+import Foundation
+
+class QuoteAPIClient {
+    
+    func getQuotes(completion: () -> Void) {
+        
+        let urlString = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
+        
+    }
+    
+    
+    
+    
+    
+}
