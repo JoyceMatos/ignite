7ba1bac106f2c9e99c75f8bdc84ad9f63e42623d
JoyceMatos <j.matosjoyce@gmail.com>
2016-12-29 20:13:18 -0500

    Make API Client

--- QuotesForYou/QuoteAPIClient.swift
+++ QuotesForYou/QuoteAPIClient.swift
@@ -10,10 +10,26 @@ import Foundation
 
 class QuoteAPIClient {
     
-    func getQuotes(completion: () -> Void) {
+    class func getQuotes(completion: @escaping ([String : String]) -> Void) {
         
         let urlString = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
         
+        guard let url = URL(string: urlString) else { print("no url- leaving function"); return }
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else { print("Unable to get data: \(error?.localizedDescription)"); return }
+            
+            if let responseJSON = try? JSONSerialization.jsonObject(with: data, options: []) as? [String : String] {
+                
+                if let responseJSON = responseJSON {
+                    completion(responseJSON)
+                }
+            }
+        }
+        task.resume()        
     }
     
     
--- QuotesForYou/ViewController.swift
+++ QuotesForYou/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        QuoteAPIClient.getQuotes { (quote) in
+            
+            print(quote)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
