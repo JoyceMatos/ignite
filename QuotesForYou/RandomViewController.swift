67285889fbd1f119d820eedcb8a75186f1a55c0e
JoyceMatos <j.matosjoyce@gmail.com>
2017-02-07 16:06:01 -0500

    Work on generating new quote

---
+++ QuotesForYou/RandomViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RandomQuoteViewController.swift
+//  QuotesForYou
+//
+//  Created by Joyce Matos on 2/7/17.
+//  Copyright © 2017 Joyce Matos. All rights reserved.
+//
+
+import UIKit
+
+class RandomViewController: UIViewController {
+    
+    let store = QuoteDataStore.shared
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+    @IBAction func newQuoteButton(_ sender: Any) {
+        self.store.getQuotes {
+            print("GETTING CALLED")
+            DispatchQueue.main.async {
+                guard let quote = self.store.quote?.quote else { print("no quote to generate- bye"); return }
+                guard let author = self.store.quote?.author else { print("no quote to generate- bye"); return }
+                self.quoteLabel.text = quote
+                self.authorLabel.text = author
+            }
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+}
