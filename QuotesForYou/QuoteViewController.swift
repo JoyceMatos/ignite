f5ec5b75a60fa67a5fe2ece078aea681bafad1bf
JoyceMatos <j.matosjoyce@gmail.com>
2017-03-17 23:30:46 -0400

    Update constraints

--- QuotesForYou/QuoteViewController.swift
+++ QuotesForYou/QuoteViewController.swift
@@ -24,7 +24,6 @@ class QuoteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initializeGesture()
         beginToObserve()
     }
     
